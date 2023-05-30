254b56b524474ab0555a29276fe03e2986337029
sikicode <siqicao811@gmail.com>
2023-05-30 16:11:59 -0700

    create segment tree md

---
+++ data_structure/trees/segment_tree.py
@@ -0,0 +1,2 @@
+# Use Cases: return sum between a range / update value for O(logn)
+# For each node at index i, the left child is at index (2*i+1), right child at (2*i+2) and the parent is at  (⌊(i – 1) / 2⌋).
