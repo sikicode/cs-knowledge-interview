432b80b604cbcd4e136474e2c5c8152e228e9823
sikicode <siqicao811@gmail.com>
2023-06-01 10:24:04 -0700

    bs

--- algorithms/binary_search/binary_search.py
+++ algorithms/binary_search/binary_search.py
@@ -1,2 +1,14 @@
 # 3 conditions 写法: https://stackoverflow.com/questions/30928268/binary-search-condition
 # Iteration vs recursion: http://www.cs.cornell.edu/courses/cs211/2006sp/Lectures/L06-Induction/binary_search.html
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
