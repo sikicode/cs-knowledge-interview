2c59dcbc2676ca21aadf011ae29e2472b1ce8419
sikicode <siqicao811@gmail.com>
2023-06-01 10:22:46 -0700

    bs conditions

---
+++ algorithms/binary_search/binary_search.py
@@ -0,0 +1,2 @@
+# 3 conditions 写法: https://stackoverflow.com/questions/30928268/binary-search-condition
+# Iteration vs recursion: http://www.cs.cornell.edu/courses/cs211/2006sp/Lectures/L06-Induction/binary_search.html
