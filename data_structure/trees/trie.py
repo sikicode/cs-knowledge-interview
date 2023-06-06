617f2ef0f77ca28ce75f41420a29ceed6c9247c8
sikicode <siqicao811@gmail.com>
2023-06-05 19:18:26 -0700

    trie

--- data_structure/trees/trie.py
+++ data_structure/trees/trie.py
@@ -1,2 +1,44 @@
 # https://leetcode.com/problems/implement-trie-prefix-tree/
+# Time O(N*avgL) for building, insertion and lookup (# of strs * # of avg word length), S(N*avgL)
 # Use cases: autocomplete, spellchecker
+# https://sikicode.com/2023/06/05/inverted-index-search-keywords-in-a-list-of-documents/
+class TrieNode:
+    def __init__(self):
+        # Stores children nodes and whether node is the end of a word
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        # Insert character by character into trie
+        for c in word:
+            # if character path does not exist, create it
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isEnd = True
+
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        # Search character by character in trie
+        for c in word:
+            # if character path does not exist, return False
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isEnd
+
+
+    def startsWith(self, prefix: str) -> bool:
+        # Same as search, except there is no isEnd condition at final return
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
