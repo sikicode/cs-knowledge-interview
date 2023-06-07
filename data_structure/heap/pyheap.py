92859238607e5122f3a7a01cc78161c375c42d22
sikicode <siqicao811@gmail.com>
2023-06-06 21:22:14 -0700

    heap time complexity

--- data_structure/heap/pyheap.py
+++ data_structure/heap/pyheap.py
@@ -1,4 +1,6 @@
 # resource: https://www.geeksforgeeks.org/heap-data-structure/
+# time complexity: insert/delete O(lgN), lookup O(1), heapify O(N)
+# heapify time: https://stackoverflow.com/questions/9755721/how-can-building-a-heap-be-on-time-complexity
 from collections import heapq
 heap = heapq.heapify([])
 heapq.heappush(heap, 1)
